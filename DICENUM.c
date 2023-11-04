695db8e4fd7bf6c147e5a8ab3bd63c16c21cbc34
Akshaan <149821111+grilledcheesecode@users.noreply.github.com>
2023-11-05 01:52:42 +0530

    Dice Number

---
+++ DICENUM.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a[6], i,p,q;
+	    int count1=0, count2=0;
+	    for(i=0;i<6;i++)
+	    {
+	        scanf("%d",&a[i]);
+	        
+	    }
+	       
+	    for (i = 0; i < 3; ++i) 
+        {
+            for (int j = i + 1; j < 3; ++j) 
+            {
+                if (a[i] < a[j]) 
+                {
+                    p = a[i];
+                    a[i] = a[j];                //sorting in descending order
+                    a[j] = p;
+                }
+            }
+            count1= count1*10+a[i];             //converting elements of array into numbers
+        }
+        for (i = 3; i < 6; ++i) 
+        {
+            for (int j = i + 1; j < 6; ++j) 
+            {
+                if (a[i] < a[j]) 
+                {
+                    q = a[i];
+                    a[i] = a[j];
+                    a[j] = q;
+                }
+            }
+            count2= count2*10+a[i];
+        }
+	    if(count1==count2) printf("tie\n");
+	    else if(count1>count2) printf("alice\n");
+	    else printf("bob\n"); 
+	}
+	return 0;
+}
